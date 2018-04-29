1d9ade73c836807a7bc1e44777e9bcec209d30ab
Georg Jaehnig <georg@jaehnig.org>
2018-04-29 22:20:00 +0200

    Display query from params.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function init() {
   var params = getSearchParameters()
   switch (params.status) {
     case 'not_found':
+      document.querySelector('#query').value = params.query;
       break;
   }
 }
