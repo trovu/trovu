30658de2f6da32f9207da56559c505b60214f301
Georg Jaehnig <georg@jaehnig.org>
2018-04-29 15:01:27 +0200

    Get placeholders.

--- main.js
+++ main.js
@@ -55,6 +55,75 @@ function buildFetchUrl(namespace, keyword, argumentCount) {
   return fetchUrl;
 }
 
+/**
+ * Get argument names from a string.
+ *
+ * @param {string} str    - The string containing placeholders.
+ * @param {string} prefix - The prefix of the placeholders. Must be Regex-escaped.
+ *
+ * @return {object} placeholders - Array keyed with the arguments names and with an array of corresponding placeholders.
+ *
+ *   If the placeholder with the same name occurs multiple times, there are also
+ *   multiple arrays in the nested array. 
+ *
+ *   Example: 
+ *     http://{%first|type=foo}{%first|type=bar}
+ *   becomes:
+ *   Array 
+ *       (
+ *            [first] => Array
+ *            (
+ *                [{%first|type=foo}] => Array
+ *                    (
+ *                        [type] => foo
+ *                    )
+ *                [{%first|type=bar}] => Array
+ *                    (
+ *                        [type] => bar
+ *                    )
+ *            )
+ *        )
+ */
+function getPlaceholdersFromString(str, prefix) {
+
+	var re = RegExp('{' + prefix + '(.+?)}', 'g');
+	var match;
+	var placeholders = {};
+
+	do {
+		match = re.exec(str);
+		if (!match) {
+		  break;
+		}
+
+    // Example value:
+    // match[1] = 'query|encoding=utf-8|another=attribute'
+    var nameAndAttributes = match[1].split('|');
+
+    // Example value:
+    // name = 'query'
+    var name = nameAndAttributes.shift();
+
+    var placeholder = {};
+    // Example value:
+    // name_and_attributes = ['encoding=utf-8'.'another=attribute']
+    for (attrStr of nameAndAttributes) {
+      [attrName, attrValue] = attrStr.split('=', 2);
+      placeholder[attrName] = attrValue;
+    }
+		placeholders[name] = placeholders[name] || {};
+    placeholders[name][match[0]] = placeholder;
+
+	} while (match);
+
+  return placeholders;
+}
+
+function getArgumentsFromString(str) {
+	return getPlaceholdersFromString(str, '%')
+}
+
+
 async function processCall() {
 
   var params = getSearchParameters();
