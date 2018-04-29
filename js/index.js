52a3b0cc0d3c1ead5bec8cb64c9dd9b7898ff0b6
Georg Jaehnig <georg@jaehnig.org>
2018-04-29 22:28:37 +0200

    Add alert and show if not found.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ function init() {
   switch (params.status) {
     case 'not_found':
       document.querySelector('#query').value = params.query;
+      document.querySelector('#alert').removeAttribute('hidden');
+      document.querySelector('#alert').textContent = 'Could not find a matching shortcut for this query.';
       break;
   }
 }
