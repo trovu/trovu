b4cd41e6680eeb962819a3ac80ba53d1c5010aea
Georg Jaehnig <georg@jaehnig.org>
2018-05-19 02:06:30 +0200

    Catch empty string.

--- js/shared.js
+++ js/shared.js
@@ -5,6 +5,9 @@ function decodeURIComponentWithPlus(str) {
 }
 
 function splitKeepRemainder(string, delimiter, n) {
+  if (!string) {
+    return [];
+  }
   var parts = string.split(delimiter);
   return parts.slice(0, n - 1).concat([parts.slice(n - 1).join(delimiter)]);
 }
