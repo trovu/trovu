7282c02950cd8c612334c74edca83fcba29aa41e
Georg Jaehnig <georg@jaehnig.org>
2018-05-19 02:09:26 +0200

    Always read query.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 function init() {
   var params = getSearchParameters()
+  document.querySelector('#query').value = params.query || "";
   switch (params.status) {
     case 'not_found':
-      document.querySelector('#query').value = params.query;
       document.querySelector('#alert').removeAttribute('hidden');
       document.querySelector('#alert').textContent = 'Could not find a matching shortcut for this query.';
       break;
