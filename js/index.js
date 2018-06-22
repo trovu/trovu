8b261fb50abbb47cea3415d43716b5b5c3bb1b97
Georg Jaehnig <georg@jaehnig.org>
2018-06-22 22:11:15 +0200

    Refactor displaySettings().

--- js/index.js
+++ js/index.js
@@ -10,23 +10,6 @@ document.querySelector('body').onload = function(event) {
   // Set query into input.
   document.querySelector('#query').value = env.query;
 
-  // Show namespaces and their template URLs.
-  for (i in env.namespaces) {
-    let liElement = document.createElement('li');
-    if (env.namespaces[i] in env.namespaceUrlTemplates) {
-      liElement.setAttribute('title', env.namespaceUrlTemplates[env.namespaces[i]]);
-      liElement.setAttribute('class', 'badge badge-primary');
-    }
-    else {
-      liElement.setAttribute('class', 'badge badge-secondary');
-    }
-    liElement.textContent = env.namespaces[i];
-    document.querySelector('ol.namespaces').append(liElement);
-  }
-
-  document.querySelector('.language.value').textContent = env.language;
-  document.querySelector('.country.value').textContent = env.country;
-
   // Show info alerts.
   switch (params.status) {
     case 'not_found':
@@ -35,21 +18,6 @@ document.querySelector('body').onload = function(event) {
       break;
   }
 
-  // Set Opensearch link.
-  // TODO:
-  // Why does let params not work here?
-  params = buildParams();
-  baseUrl = buildBaseUrl();
-  let urlOpensearch = baseUrl + 'opensearch/?' + jqueryParam(params);
-
-  let linkSearch = document.querySelector('#linkSearch');
-  linkSearch.setAttribute('title', 'Trovu: ' + env.namespaces.join(','));
-  linkSearch.setAttribute('href', urlOpensearch);
-
-  // Set Process URL.
-  let urlProcess = baseUrl + 'process/#query=%s&' + jqueryParam(params);
-  let preProcessUrl = document.querySelector('pre.process-url');
-  preProcessUrl.textContent = urlProcess;
 }
 
 document.getElementById('query-form').onsubmit = function(event) {
@@ -82,4 +50,39 @@ function displaySettings() {
   document.querySelector('#languageSetting').value = env.language;
   document.querySelector('#countrySetting').value = env.country;
   document.querySelector('#namespacesSetting').value = env.namespaces.join(',');
+
+  document.querySelector('ol.namespaces').innerHTML = '';
+
+  // Show namespaces and their template URLs.
+  for (i in env.namespaces) {
+    let liElement = document.createElement('li');
+    if (env.namespaces[i] in env.namespaceUrlTemplates) {
+      liElement.setAttribute('title', env.namespaceUrlTemplates[env.namespaces[i]]);
+      liElement.setAttribute('class', 'badge badge-primary');
+    }
+    else {
+      liElement.setAttribute('class', 'badge badge-secondary');
+    }
+    liElement.textContent = env.namespaces[i];
+    document.querySelector('ol.namespaces').append(liElement);
+  }
+
+  document.querySelector('.language.value').textContent = env.language;
+  document.querySelector('.country.value').textContent = env.country;
+
+  // Set Opensearch link.
+  // TODO:
+  // Why does let params not work here?
+  params = buildParams();
+  baseUrl = buildBaseUrl();
+  let urlOpensearch = baseUrl + 'opensearch/?' + jqueryParam(params);
+
+  let linkSearch = document.querySelector('#linkSearch');
+  linkSearch.setAttribute('title', 'Trovu: ' + env.namespaces.join(','));
+  linkSearch.setAttribute('href', urlOpensearch);
+
+  // Set Process URL.
+  let urlProcess = baseUrl + 'process/#query=%s&' + jqueryParam(params);
+  let preProcessUrl = document.querySelector('pre.process-url');
+  preProcessUrl.textContent = urlProcess;
 }
