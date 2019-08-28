e978415fdc295b92fbfb100603286f07a5fdb72a
Georg Jaehnig <georg@jaehnig.org>
2019-08-28 14:50:15 +0200

    let instead of var.

--- js/helper.js
+++ js/helper.js
@@ -4,7 +4,7 @@ export default class Helper {
     if (!string) {
       return [];
     }
-    var parts = string.split(delimiter);
+    let parts = string.split(delimiter);
     return parts.slice(0, n - 1).concat([parts.slice(n - 1).join(delimiter)]);
   }
 
