3a24ed89054e238ff206f96af3e3909d92d364cb
Georg Jaehnig <georg@jaehnig.org>
2019-09-03 22:20:35 +0200

    Cut last &.

--- __tests__/query.test.js
+++ __tests__/query.test.js
@@ -2,7 +2,7 @@ const docroot = 'http://l.tro/process/index.html?#debug=1&';
 
 const tests = [
   {
-    queryStr: 'language=de&country=de&query=db b%2Cm%2C8%2Cmo&',
+    queryStr: 'language=de&country=de&query=db b%2Cm%2C8%2Cmo',
     expectedRedirectUrl: 'http://reiseauskunft.bahn.de/bin/query.exe/d?S=Berlin&Z=M%C3%BCnchen&time=08%3A00&date=09.09.2019&timesel=depart&start=1',
   },
 ];
