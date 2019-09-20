0d0b47e29e84334c05abb968e9e721791bf7b586
Georg Jaehnig <georg@jaehnig.org>
2019-09-20 23:11:20 +0200

    Add more tests.

--- __tests__/env.test.js
+++ __tests__/env.test.js
@@ -10,3 +10,19 @@ env.getNavigatorLanguage = () => {
 test('getNavigatorLanguage', () => {
   expect(env.getNavigatorLanguage()).toMatch('en-uk');
 } );
+
+test('getLanguageAndCountryFromBrowser', () => {
+  expect(env.getLanguageAndCountryFromBrowser()).toEqual({language: 'en', country: 'uk'});
+} );
+
+test('getDefaultLanguageAndCountry', () => {
+  expect(env.getDefaultLanguageAndCountry()).toEqual({language: 'en', country: 'uk'});
+} );
+
+test('getDefaultLanguageAndCountry when navigator.language empty', () => {
+  env.getNavigatorLanguage = () => {
+    const languageStr = '';
+    return languageStr;
+  }
+  expect(env.getDefaultLanguageAndCountry()).toEqual({language: 'en', country: 'us'});
+} );
