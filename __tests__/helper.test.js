9b3043a582aa5183e4facb6b76b9bf731f49c7dc
Georg Jaehnig <georg@jaehnig.org>
2019-09-22 00:49:12 +0200

    Prettier.

--- __tests__/helper.test.js
+++ __tests__/helper.test.js
@@ -1,19 +1,25 @@
 import Helper from "../public/js/helper.js";
 
-  const getUrlHashFooBar = () => {
-  const hash = 'foo=bar&baz=boo'
+const getUrlHashFooBar = () => {
+  const hash = "foo=bar&baz=boo";
   return hash;
-}
+};
 
 test("jqueryDeparam", () => {
-  expect(Helper.jqueryDeparam('foo=bar&baz=boo')).toEqual({foo: 'bar', baz: 'boo' });
+  expect(Helper.jqueryDeparam("foo=bar&baz=boo")).toEqual({
+    foo: "bar",
+    baz: "boo"
+  });
 });
 
 test("getParams", () => {
   Helper.getUrlHash = getUrlHashFooBar;
-  expect(Helper.getParams()).toEqual({foo: 'bar', baz: 'boo' });
+  expect(Helper.getParams()).toEqual({ foo: "bar", baz: "boo" });
 });
 
 test("splitKeepRemainder", () => {
-  expect(Helper.splitKeepRemainder('g foo, bar', ' ', 2)).toEqual(['g', 'foo, bar']);
+  expect(Helper.splitKeepRemainder("g foo, bar", " ", 2)).toEqual([
+    "g",
+    "foo, bar"
+  ]);
 });
