1887f0270709b58c6c510e146967405f817c51cb
Georg Jaehnig <georg@jaehnig.org>
2020-09-18 14:58:04 +0200

    Set fallback only when github is not set.

--- public/opensearch/index.php
+++ public/opensearch/index.php
@@ -10,16 +10,15 @@ $urlTrovu      = str_replace('opensearch/', '', $urlOpensearch);
 // Get environment from GET params.
 $env = $_GET;
 
-// Set fallback values.
-$env['language'] = $env['language'] ?? 'en';
-$env['country']  = $env['country']  ?? 'us';
-
 $title = 'Trovu: ';
 
 if (!empty($env['github'])) {
   $title .=  $env['github'];
 }
 else {
+  // Set fallback values.
+  $env['language'] = $env['language'] ?? 'en';
+  $env['country']  = $env['country']  ?? 'us';
   $title .=  $env['language'] . '-' . strtoupper($env['country']);
 }
 
