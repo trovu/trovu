142a6eddca07cb0ce570cf352b7e95dfcf43954b
Georg Jaehnig <georg@jaehnig.org>
2020-10-03 18:29:15 +0200

    Start using awesomeplete.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import jsyaml from "js-yaml";
 import countriesList from "countries-list";
 import { detect, browserName } from "detect-browser";
 import BSN from "bootstrap.native/dist/bootstrap-native.esm.min.js";
+import awesomeplete from "awesomplete";
 
+import "awesomplete/awesomplete.css";
 import "./css/style.css";
 
 import Helper from "./helper.js";
