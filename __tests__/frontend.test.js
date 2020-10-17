9c673f28a6939ebab061e9ea761a880104651bde
Georg Jaehnig <georg@jaehnig.org>
2020-10-17 22:44:59 +0200

    Add frontend tests.

---
+++ __tests__/frontend.test.js
@@ -0,0 +1,17 @@
+const docroot = "http://l.tro:8080/#language=en&country=us";
+
+describe("Google", () => {
+  beforeAll(async () => {
+    await page.goto(docroot, { waitUntil: "networkidle0" });
+  });
+
+  it('should be titled "trovu"', async () => {
+    await expect(page.title()).resolves.toMatch("trovu");
+  });
+
+  it("should have Suggestions", async () => {
+    await page.focus("#query");
+    await page.keyboard.type("g");
+    await expect(page.content()).resolves.toMatch("Google.com");
+  });
+});
--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,4 @@
 jest env -c jest.config.js
 jest helper -c jest.config.js
 jest calls -c jest.config.with-puppeteer.js
+jest frontend -c jest.config.with-puppeteer.js
