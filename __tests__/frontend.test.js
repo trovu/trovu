9677c2925f8c832848a277ed8a1b15696d99a9c8
Georg Jaehnig <georg@jaehnig.org>
2020-10-18 21:36:27 +0200

    Add 'should have a working Settings modal'.

--- __tests__/frontend.test.js
+++ __tests__/frontend.test.js
@@ -14,4 +14,13 @@ describe("Trovu homepage", () => {
     await page.keyboard.type("g");
     await expect(page.content()).resolves.toMatch("Google.com");
   });
+
+  it("should have a working Settings modal", async () => {
+    await page.click("#settings-button");
+    await page.select("#languageSetting", "pl");
+    await page.waitFor(500); // TODO: Find cleaner solution.
+    await page.click("#settingsSave"),
+    await page.waitFor(500); // TODO: Find cleaner solution.
+    expect(await page.evaluate('location.hash')).toMatch(new RegExp('language=pl'));
+  });
 });
