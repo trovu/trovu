e5d8cb1daf8a068c32acb1e794b99a13696dac33
Georg Jaehnig <georg@jaehnig.org>
2020-10-19 01:55:30 +0200

    Refactor home.js.

---
+++ src/home.js
@@ -0,0 +1,100 @@
+/** @module Home */
+
+import BSN from "bootstrap.native/dist/bootstrap-native.esm.min.js";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./scss/style.scss";
+
+import Helper from "./helper.js";
+import Env from "./env.js";
+import Suggestions from "./suggestions";
+import Settings from "./settings.js";
+import AddToBrowser from "./addToBrowser.js";
+
+/** Set and manage the homepage. */
+
+export default class Home {
+  constructor() {
+  }
+
+  async initialize() {
+    this.env = new Env();
+
+    // Must be done before env.populate()
+    // otherwise Chrome does not autodiscover.
+    AddToBrowser.addLinkSearch();
+
+    // Init environment.
+    await this.env.populate();
+
+    this.showInfoAlerts();
+    new Settings(this.env);
+    new AddToBrowser(this.env);
+    this.setLocationHash();
+    this.setQueryElement();
+    document.getElementById("query-form").onsubmit = this.submitQuery;
+  }
+
+  /**
+   * Get the URL to the Process script.
+   */
+  getProcessUrl() {
+    const params = this.env.getParams();
+    params["query"] = document.getElementById("query").value;
+
+    const paramStr = Helper.getUrlParamStr(params);
+    const processUrl = "process/index.html?#" + paramStr;
+
+    return processUrl;
+  }
+
+  setQueryElement() {
+    // Set query into input.
+    document.querySelector("#query").value = this.env.query || "";
+    new Suggestions(this.env.namespaces, this.submitQuery);
+    document.querySelector("#query").focus();
+  }
+
+  setLocationHash() {
+    const paramStr = this.env.getParamStr();
+    window.location.hash = "#" + paramStr;
+  }
+
+  /**
+   * Show custom alerts above query input.
+   */
+  showInfoAlerts() {
+    const params = Helper.getUrlParams();
+
+    // Show info alerts.
+    switch (params.status) {
+      case "not_found":
+        document.querySelector("#alert").removeAttribute("hidden");
+        document.querySelector("#alert").textContent =
+          "Could not find a matching shortcut for this query.";
+        break;
+      case "reloaded":
+        document.querySelector("#alert").removeAttribute("hidden");
+        document.querySelector("#alert").textContent =
+          "Shortcuts were reloaded in all namespaces.";
+        break;
+    }
+  }
+
+  /**
+   * On submitting the query.
+   *
+   * @param {object} event – The submitting event.
+   */
+  submitQuery = (event) => {
+    // Prevent default sending as GET parameters.
+    if (event) {
+      event.preventDefault();
+    }
+
+    const processUrl = this.getProcessUrl();
+
+    // Redirect to process script.
+    window.location.href = processUrl;
+  }
+}
