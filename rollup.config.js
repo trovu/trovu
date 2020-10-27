a4b4d90ccb5efa3dd2d724fb594434e916610c8c
Georg Jaehnig <georg@jaehnig.org>
2020-10-27 11:15:21 +0300

    Refactor common.

--- rollup.config.js
+++ rollup.config.js
@@ -4,40 +4,33 @@ import json from "@rollup/plugin-json";
 import scss from "rollup-plugin-scss";
 import { terser } from "rollup-plugin-terser";
 
+const common = {
+  output: {
+    dir: "public/bundle/",
+    format: "iife",
+    name: "index",
+    //entryFileNames: "[name].[hash].js",
+    entryFileNames: "[name].js",
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    json(),
+    scss({
+      output: "public/bundle/style.css",
+      outputStyle: process.env.BUILD === "production" ? "compressed" : "",
+    }),
+    process.env.BUILD === "production" && terser(),
+  ],
+};
+
 export default [
   {
     input: "src/js/index.js",
-    output: {
-      dir: "public/bundle/",
-      format: "iife",
-      name: "index",
-      //entryFileNames: "[name].[hash].js",
-      entryFileNames: "[name].js",
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      json(),
-      scss({
-        output: "public/bundle/style.css",
-        outputStyle: process.env.BUILD === "production" ? "compressed" : "",
-      }),
-      process.env.BUILD === "production" && terser(),
-    ],
+    ...common,
   },
   {
     input: "src/js/process.js",
-    output: {
-      dir: "public/bundle/",
-      format: "iife",
-      name: "process",
-      entryFileNames: "[name].js",
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      json(),
-      process.env.BUILD === "production" && terser(),
-    ],
+    ...common,
   },
 ];
