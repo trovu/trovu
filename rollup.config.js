19f5350414c701c19af42beefe56ddcd9da23413
georgjaehnig <georg@jaehnig.org>
2021-01-23 15:54:11 +0100

    Use generic placeholder.

--- rollup.config.js
+++ rollup.config.js
@@ -8,66 +8,63 @@ import { readFileSync } from "fs";
 
 const isProduction = process.env.BUILD === "production";
 
-export default [ {
-  input: "src/js/process.js",
-  output: {
-    dir: "dist/public/",
-    name: "process",
-    entryFileNames: "[name].[hash].js",
-    sourcemap: true,
+export default [
+  {
+    input: "src/js/process.js",
+    output: {
+      dir: "dist/public/",
+      name: "process",
+      entryFileNames: "[name].[hash].js",
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      json(),
+      scss({
+        output: "dist/public/style.css",
+        outputStyle: isProduction ? "compressed" : "",
+      }),
+      isProduction && terser(),
+      html({
+        fileName: "process/index.html",
+        template: ({ attributes, bundle, files, publicPath, title }) => {
+          console.log(Object.keys(bundle));
+          const [processJs] = Object.keys(bundle);
+          const htmlTemplate = readFileSync("src/html/process.html").toString();
+          const html = htmlTemplate.replace("{{fileNameJs}}", processJs);
+          return html;
+        },
+      }),
+    ],
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    json(),
-    scss({
-      output: "dist/public/style.css",
-      outputStyle: isProduction ? "compressed" : "",
-    }),
-    isProduction && terser(),
-    html({
-      fileName: "process/index.html",
-      template: ({ attributes, bundle, files, publicPath, title }) => {
-        //console.log('files:', files);
-        console.log(Object.keys(bundle));
-        //console.log('template process');
-        const [processJs] = Object.keys(bundle);
-        const fileNames = Object.keys(bundle);
-        const htmlTemplate = readFileSync("src/html/process.html").toString();
-        const html = htmlTemplate.replace("{{processJs}}", processJs);
-        return html;
-      },
-    })
-  ],
-},{
-  input: "src/js/index.js",
-  output: {
-    dir: "dist/public/",
-    name: "process",
-    entryFileNames: "[name].[hash].js",
-    sourcemap: true,
+  {
+    input: "src/js/index.js",
+    output: {
+      dir: "dist/public/",
+      name: "process",
+      entryFileNames: "[name].[hash].js",
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      json(),
+      scss({
+        output: "dist/public/style.css",
+        outputStyle: isProduction ? "compressed" : "",
+      }),
+      isProduction && terser(),
+      html({
+        fileName: "index.html",
+        template: ({ attributes, bundle, files, publicPath, title }) => {
+          console.log(Object.keys(bundle));
+          const [indexJs] = Object.keys(bundle);
+          const htmlTemplate = readFileSync("src/html/index.html").toString();
+          const html = htmlTemplate.replace("{{fileNameJs}}", indexJs);
+          return html;
+        },
+      }),
+    ],
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    json(),
-    scss({
-      output: "dist/public/style.css",
-      outputStyle: isProduction ? "compressed" : "",
-    }),
-    isProduction && terser(),
-    html({
-      fileName: "index.html",
-      template: ({ attributes, bundle, files, publicPath, title }) => {
-        //console.log('files:', files);
-        console.log(Object.keys(bundle));
-        //console.log('template process');
-        const [indexJs] = Object.keys(bundle);
-        const fileNames = Object.keys(bundle);
-        const htmlTemplate = readFileSync("src/html/index.html").toString();
-        const html = htmlTemplate.replace("{{indexJs}}", indexJs);
-        return html;
-      },
-    })
-  ],
-}];
+];
