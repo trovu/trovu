6a040702a99d044f5a39b33443c5330707c8bf19
Georg Jähnig <georg@jaehnig.org>
2022-12-19 01:23:45 +0100

    Allow to switch country on call with city.

--- src/js/modules/type/city.js
+++ src/js/modules/type/city.js
@@ -5,13 +5,20 @@ export default class CityType {
   static async parse(str, country, reload, debug) {
     const fetchUrlTemplate =
       "https://data.trovu.net/data/types/city/{%country}.yml";
+    let matches;
+
+    let abbreviation = str;
+    if (matches = str.match(/^(\w\w+)(\.)(.+)$/)) {
+      [, country, , abbreviation] = matches;
+    }
+
     const fetchUrl = fetchUrlTemplate.replace("{%country}", country);
 
     const citiesYml = await Helper.fetchAsync(fetchUrl, reload, debug);
     if (citiesYml) {
       const cities = jsyaml.load(citiesYml);
-      if (str in cities) {
-        const city = cities[str];
+      if (abbreviation in cities) {
+        const city = cities[abbreviation];
         return city;
       }
     }
