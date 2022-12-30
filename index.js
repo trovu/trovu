d5801d77b4ff8a413e589d458261046374275269
Georg Jähnig <georg@jaehnig.org>
2022-12-31 00:52:29 +0100

    Add isValidUrl().

--- index.js
+++ index.js
@@ -44,8 +44,44 @@ actions['normalize'] = async function () {
     writeYmls(ymls);
 }
 
-actions['removeDeadDomains'] = async function() {
-    const ymls = loadYmls();
+const isValidUrl = urlString=> {
+    try { 
+        return Boolean(new URL(urlString)); 
+    }
+    catch(e){ 
+        return false; 
+    }
+}
+
+actions['removeDeadDomains'] = async function () {
+    const ymlsAll = loadYmls();
+    const ymls = {};
+    const ymlFilePath = '/Users/jrg/cde/web/tro/trovu-data/shortcuts/old-o.yml';
+    ymls[ymlFilePath] = ymlsAll[ymlFilePath];
+    for (const ymlFilePath in ymls) {
+        const yml = ymls[ymlFilePath];
+        for (const key in yml) {
+            const shortcut = yml[key];
+            console.log(shortcut);
+            if (!isValidUrl(shortcut.url)) {
+                console.log(shortcut.url + ' is not a valid url, skipping.');
+                continue;
+            }
+            const url = new URL(shortcut.url);
+            const testUrl = url.protocol + '//' + url.host;
+            try {
+                const response = await fetch(testUrl);
+                console.log(response.status, testUrl);
+                if (response.status != 200) {
+                    delete yml[key];
+                }
+            }
+            catch (error) {
+                console.error(error);
+                delete yml[key];
+            }
+        }
+    }
     writeYmls(ymls);
 }
 
