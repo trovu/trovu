b06ceb1e69f02cf2a36334c1688b2c2064253dff
Georg Jähnig <georg@jaehnig.org>
2023-01-01 17:44:16 +0100

    Reorder back.

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const actions = {};
 
 let ymlDirPath;
 
-main();
-
 async function main() {
   if (process.argv.length < 3) {
     console.log('Usage: node index.js action [path]')
@@ -118,3 +116,4 @@ actions['removeDeadDomains'] = async function () {
   }
   writeYmls(ymls);
 }
+main();
