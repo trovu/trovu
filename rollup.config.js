8645ec7940dbb33ee8e615062b79015b4ad98325
Georg Jähnig <georg@jaehnig.org>
2023-01-03 15:54:26 +0100

    Format.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import scss from "rollup-plugin-scss";
-import { terser } from "rollup-plugin-terser";
-import html from "@rollup/plugin-html";
-import { readFileSync } from "fs";
-import copy from "rollup-plugin-copy";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import scss from 'rollup-plugin-scss';
+import { terser } from 'rollup-plugin-terser';
+import html from '@rollup/plugin-html';
+import { readFileSync } from 'fs';
+import copy from 'rollup-plugin-copy';
 
-const isProduction = process.env.BUILD === "production";
+const isProduction = process.env.BUILD === 'production';
 
 const output = {
-  dir: "dist/public/",
-  name: "process",
-  entryFileNames: "[name].[hash].js",
+  dir: 'dist/public/',
+  name: 'process',
+  entryFileNames: '[name].[hash].js',
   sourcemap: true,
 };
 
@@ -20,7 +20,7 @@ const template = (templateFilePath) => {
   const templateFunc = ({ attributes, bundle, files, publicPath, title }) => {
     const [fileNameJs] = Object.keys(bundle);
     const htmlTemplate = readFileSync(templateFilePath).toString();
-    const html = htmlTemplate.replace("{{fileNameJs}}", fileNameJs);
+    const html = htmlTemplate.replace('{{fileNameJs}}', fileNameJs);
     return html;
   };
   return templateFunc;
@@ -28,28 +28,28 @@ const template = (templateFilePath) => {
 
 export default [
   {
-    input: "src/js/index.js",
+    input: 'src/js/index.js',
     output: output,
     plugins: [
       resolve(),
       commonjs(),
       json(),
       scss({
-        output: "dist/public/style.css",
-        outputStyle: isProduction ? "compressed" : "expanded",
+        output: 'dist/public/style.css',
+        outputStyle: isProduction ? 'compressed' : 'expanded',
       }),
       isProduction && terser(),
       html({
-        fileName: "index.html",
-        template: template("src/html/index.html"),
+        fileName: 'index.html',
+        template: template('src/html/index.html'),
       }),
       copy({
-        targets: [{ src: "src/opensearch/", dest: "dist/public/" }],
+        targets: [{ src: 'src/opensearch/', dest: 'dist/public/' }],
       }),
     ],
   },
   {
-    input: "src/js/process.js",
+    input: 'src/js/process.js',
     output: output,
     plugins: [
       resolve(),
@@ -57,8 +57,8 @@ export default [
       json(),
       isProduction && terser(),
       html({
-        fileName: "process/index.html",
-        template: template("src/html/process.html"),
+        fileName: 'process/index.html',
+        template: template('src/html/process.html'),
       }),
     ],
   },
