483a774d271f15916778a9b76117638f7e4ae83f
Georg Jähnig <georg@jaehnig.org>
2023-01-10 20:00:26 +0100

    Format.

--- __tests__/frontend.test.js
+++ __tests__/frontend.test.js
@@ -1,53 +1,53 @@
-const docroot = "http://127.0.0.1:8081/";
+const docroot = 'http://127.0.0.1:8081/';
 
-describe("Trovu homepage", () => {
-
+describe('Trovu homepage', () => {
   beforeAll(async () => {
-    await page.goto(docroot, { waitUntil: "networkidle0" });
+    await page.goto(docroot, { waitUntil: 'networkidle0' });
   });
 
-  it("should redirect to hash with language and country", async () => {
-    expect(await page.evaluate("location.hash")).toMatch(
-      new RegExp("country=gb&language=en")
+  it('should redirect to hash with language and country', async () => {
+    expect(await page.evaluate('location.hash')).toMatch(
+      new RegExp('country=gb&language=en'),
     );
   });
 
   it('should be titled "trovu"', async () => {
-    await expect(page.title()).resolves.toMatch("trovu");
+    await expect(page.title()).resolves.toMatch('trovu');
   });
 
-  it("should contain an Opensearch tag", async () => {
+  it('should contain an Opensearch tag', async () => {
     await page.waitFor(1000); // TODO: Find cleaner solution.
-    await expect(page.content()).resolves.toMatch('<link rel="search" type="application/opensearchdescription+xml" href="/opensearch/?country=gb&amp;language=en" title="Trovu">');
-  });
-
-  it("should have Suggestions", async () => {
-    await page.focus("#query");
-    await page.keyboard.type("g");
-    await page.waitFor(2000); // TODO: Find cleaner solution.
-    await expect(page.content()).resolves.toMatch("Google");
-  });
-
-  it("should have a working Settings modal", async () => {
-    await page.click("#settings-button");
-    await page.select("#languageSetting", "pl");
-    await page.waitFor(1000); // TODO: Find cleaner solution.
-    await page.click("#settingsSave"), await page.waitFor(500); // TODO: Find cleaner solution.
-    expect(await page.evaluate("location.hash")).toMatch(
-      new RegExp("language=pl")
+    await expect(page.content()).resolves.toMatch(
+      '<link rel="search" type="application/opensearchdescription+xml" href="/opensearch/?country=gb&amp;language=en" title="Trovu">',
     );
   });
 
+  it('should have Suggestions', async () => {
+    await page.focus('#query');
+    await page.keyboard.type('g');
+    await page.waitFor(2000); // TODO: Find cleaner solution.
+    await expect(page.content()).resolves.toMatch('Google');
+  });
+
+  it('should have a working Settings modal', async () => {
+    await page.click('#settings-button');
+    await page.select('#languageSetting', 'pl');
+    await page.waitFor(1000); // TODO: Find cleaner solution.
+    await page.click('#settingsSave'), await page.waitFor(500); // TODO: Find cleaner solution.
+    expect(await page.evaluate('location.hash')).toMatch(
+      new RegExp('language=pl'),
+    );
+  });
 });
 
-describe("Trovu homepage, Shortcut not found", () => {
-
-  it("should show not_found", async () => {
-    await page.goto(docroot + "#country=gb&language=en&query=foobar&status=not_found");
+describe('Trovu homepage, Shortcut not found', () => {
+  it('should show not_found', async () => {
+    await page.goto(
+      docroot + '#country=gb&language=en&query=foobar&status=not_found',
+    );
     await page.waitFor(2000);
     await expect(page.content()).resolves.toMatch(
-      "Could not find a matching shortcut for this query."
+      'Could not find a matching shortcut for this query.',
     );
   });
-
 });
