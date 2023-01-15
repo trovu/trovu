b51358374200b90298d84a7c987b5d26d7a3e0f1
Georg Jähnig <georg@jaehnig.org>
2023-01-16 00:37:28 +0100

    Remove async.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,22 +1,22 @@
 describe('template spec', () => {
-  beforeEach(async () => {
+  beforeEach(() => {
     cy.visit('/');
   });
 
-  it('should redirect to hash with language and country', async () => {
+  it('should redirect to hash with language and country', () => {
     cy.hash().should('include', 'country=gb&language=en');
   });
 
-  it('should have a title including "trovu"', async () => {
+  it('should have a title including "trovu"', () => {
     cy.title().should('include', 'trovu');
   });
 
-  it('should contain an Opensearch tag', async () => {
+  it('should contain an Opensearch tag', () => {
     cy.contains(
       '<link rel="search" type="application/opensearchdescription+xml" href="/opensearch/?country=gb&amp;language=en" title="Trovu">',
     );
   });
-  it('should have Suggestions', async () => {
+  it('should have Suggestions', () => {
     cy.get('#query').first().focus();
     cy.get('#query').type('g');
     cy.contains('Google Web Homepage');
