918f81cbda31c91d424c50b30614e109f11fbd03
Georg Jähnig <georg@jaehnig.org>
2023-01-15 22:04:14 +0100

    Transfer first tests to cypress.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,24 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  beforeEach(async () => {
+    cy.visit('/');
+  });
+
+  it('should redirect to hash with language and country', async () => {
+    cy.hash().should('include', 'country=gb&language=en');
+  });
+
+  it('should have a title including "trovu"', async () => {
+    cy.title().should('include', 'trovu');
+  });
+
+  it('should contain an Opensearch tag', async () => {
+    cy.contains(
+      '<link rel="search" type="application/opensearchdescription+xml" href="/opensearch/?country=gb&amp;language=en" title="Trovu">',
+    );
+  });
+  it('should have Suggestions', async () => {
+    cy.get('#query').first().focus();
+    cy.get('#query').type('g');
+    cy.contains('Google Web Homepage');
+  });
+});
