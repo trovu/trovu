105431bd105c26a23a6553c150ca38e98e5e1944
Georg Jähnig <georg@jaehnig.org>
2023-01-15 22:42:06 +0100

    Use 127.0.0.1.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8081',
+    baseUrl: 'http://127.0.0.1:8081/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
