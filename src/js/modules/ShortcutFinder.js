faeea1458a346ae177699a7c10e304b8d1049c47
Georg Jähnig <georg@jaehnig.org>
2023-03-03 01:17:41 +0100

    Remove unused method.

--- src/js/modules/ShortcutFinder.js
+++ src/js/modules/ShortcutFinder.js
@@ -13,30 +13,6 @@ export default class ShortcutFinder {
    *
    * @return {array} shortcuts  - The array of found shortcuts.
    */
-  static async matchShortcuts(keyword, args, namespaces, reload, debug) {
-    const shortcuts = {};
-    for (let namespace of namespaces) {
-      // If shortcuts are empty.
-      // (e.g. because of previous fetch error)
-      if (!namespace.shortcuts) {
-        continue;
-      }
-      const shortcut = namespace.shortcuts[keyword + ' ' + args.length];
-      if (shortcut) {
-        shortcuts[namespace.name] = shortcut;
-      }
-    }
-    return shortcuts;
-  }
-
-  /**
-   * Match shortcuts for keyword and args.
-   *
-   * @param {string} keyword    - The keyword of the query.
-   * @param {array} args        - The arguments of the query.
-   *
-   * @return {array} shortcuts  - The array of found shortcuts.
-   */
   static async matchShortcuts2(keyword, args, namespaceInfos, reload, debug) {
     const shortcuts = {};
 
