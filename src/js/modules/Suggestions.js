e3fe5564aab6df668852740be4cbd3e0dd06e50c
Georg Jähnig <georg@jaehnig.org>
2023-03-03 19:39:15 +0100

    Rename vars.

--- src/js/modules/Suggestions.js
+++ src/js/modules/Suggestions.js
@@ -9,8 +9,8 @@ import QueryParser from './QueryParser.js';
 /** Set and manage the Suggestions. */
 
 export default class Suggestions {
-  constructor(namespaces, submitQuery) {
-    this.namespaces = namespaces;
+  constructor(namespaceInfos, submitQuery) {
+    this.namespacesInfos = namespaceInfos;
     this.submitQuery = submitQuery;
 
     const queryInput = document.querySelector('#query');
@@ -136,8 +136,9 @@ export default class Suggestions {
       urlMiddleUnreachable: [],
     };
 
-    for (const namespace of this.namespaces) {
-      for (const shortcut of Object.values(namespace.shortcuts)) {
+    for (const namespaceInfo of Object.values(this.namespacesInfos)) {
+      console.log(namespaceInfo);
+      for (const shortcut of Object.values(namespaceInfo.shortcuts)) {
         if (shortcut.deprecated || shortcut.removed) {
           continue;
         }
