f2ff79ff7645198f402a61a9d67298b1ef926d9e
Georg Jähnig <georg@jaehnig.org>
2023-04-27 01:37:44 +0200

    Test for date of current year, and days of week.

--- src/js/modules/UrlProcessor.test.js
+++ src/js/modules/UrlProcessor.test.js
@@ -1,10 +1,23 @@
 import UrlProcessor from './UrlProcessor.js';
+import dayjs from 'dayjs';
 
 test('processTypeDate', async () => {
   const expectations = {
-    14.5: '2023-05-14',
-    '14.5.': '2023-05-14',
+    31.12: dayjs()
+      .set('month', 12 - 1)
+      .set('date', 31),
+    '31.12.': dayjs()
+      .set('month', 12 - 1)
+      .set('date', 31),
   };
+  'so mo di mi do fr sa'.split(' ').forEach((dow, index) => {
+    expectations[dow] = dayjs()
+      .day(index)
+      .add(dayjs().day() > index - 1 ? 7 : 0, 'day');
+  });
+  for (const input in expectations) {
+    expectations[input] = expectations[input].format('YYYY-MM-DD');
+  }
   const locale = 'de-DE';
   const attributes = {};
   for (const input in expectations) {
