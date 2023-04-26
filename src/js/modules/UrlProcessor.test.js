4d88e603757efcd34e5a5ea0bf66b9bee69aa249
Georg Jähnig <georg@jaehnig.org>
2023-04-27 01:12:02 +0200

    Test now for 14.5.

--- src/js/modules/UrlProcessor.test.js
+++ src/js/modules/UrlProcessor.test.js
@@ -3,6 +3,7 @@ import UrlProcessor from './UrlProcessor.js';
 test('processTypeDate', async () => {
   const expectations = {
     14.5: '2023-05-14',
+    '14.5.': '2023-05-14',
   };
   const locale = 'de-DE';
   const attributes = {};
