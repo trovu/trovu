eda109c27228c71c70016fb0b1f967dfb4bcd8e6
Georg Jähnig <georg@jaehnig.org>
2023-04-27 00:42:57 +0200

    Allow more expectations.

--- src/js/modules/UrlProcessor.test.js
+++ src/js/modules/UrlProcessor.test.js
@@ -1,5 +1,17 @@
 import UrlProcessor from './UrlProcessor.js';
 
-test('processTypeDate', () => {
-  expect(1).toEqual(1);
+test('processTypeDate', async () => {
+  const expectations = {
+    14.5: '2023-05-14',
+  };
+  const locale = 'de-DE';
+  const attributes = {};
+  for (const input in expectations) {
+    const output = await UrlProcessor.processTypeDate(
+      input,
+      locale,
+      attributes,
+    );
+    expect(output).toEqual(expectations[input]);
+  }
 });
