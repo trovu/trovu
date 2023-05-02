5facb1526ae79b11755f0f2dd6b66a5adc31289b
Georg Jähnig <georg@jaehnig.org>
2023-05-03 01:47:54 +0200

    Use .toTimeString().

--- src/js/modules/type/time.test.js
+++ src/js/modules/type/time.test.js
@@ -3,13 +3,13 @@ import TimeType from './time.js';
 test('parseTimeType', async () => {
   jest.useFakeTimers().setSystemTime(new Date(2020, 0, 1, 12, 0, 0));
   const expectations = {
-    15: new Date(2020, 0, 1, 15, 0),
-    '15:00': new Date(2020, 0, 1, 15, 0),
-    '+1': new Date(2020, 0, 1, 13, 0),
-    '-1': new Date(2020, 0, 1, 11, 0),
+    15: new Date(2020, 0, 1, 15),
+    '15:00': new Date(2020, 0, 1, 15),
+    '+1': new Date(2020, 0, 1, 13),
+    '-1': new Date(2020, 0, 1, 11),
   };
   for (const input in expectations) {
     const output = await TimeType.parse(input);
-    expect(output).toEqual(expectations[input]);
+    expect(output.toTimeString()).toEqual(expectations[input].toTimeString());
   }
 });
