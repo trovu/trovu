b18f65acac4eee8ae219a3f608eb6ea8b32b05ac
Georg Jähnig <georg@jaehnig.org>
2023-05-03 01:40:41 +0200

    Add date.test.js.

---
+++ src/js/modules/type/date.test.js
@@ -0,0 +1,23 @@
+import DateType from './date.js';
+
+test('parseDateType', async () => {
+  jest.useFakeTimers().setSystemTime(new Date(2020, 11, 31));
+  const expectations = {
+    2: new Date(2021, 0, 2),
+    '2.': new Date(2021, 0, 2),
+    22: new Date(2021, 0, 22),
+    '22.': new Date(2021, 0, 22),
+    22.11: new Date(2021, 10, 22),
+    '22.11.': new Date(2021, 10, 22),
+    '22.11.20': new Date(2020, 10, 22),
+    '22.11.2020': new Date(2020, 10, 22),
+    '11/22': new Date(2021, 10, 22),
+    '11/22/20': new Date(2020, 10, 22),
+    '+1': new Date(2021, 0, 1),
+    '-1': new Date(2020, 11, 30),
+  };
+  for (const input in expectations) {
+    const output = await DateType.parse(input);
+    expect(output.toDateString()).toEqual(expectations[input].toDateString());
+  }
+});
