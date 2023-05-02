0d1d9d3bf77cddbfaef96efedf6f5f9bdae5b1cf
Georg Jähnig <georg@jaehnig.org>
2023-05-03 01:45:25 +0200

    Add tests for days of week.

--- src/js/modules/type/date.test.js
+++ src/js/modules/type/date.test.js
@@ -16,6 +16,13 @@ test('parseDateType', async () => {
     '11/22/20': new Date(2020, 10, 22),
     '+1': new Date(2021, 0, 1),
     '-1': new Date(2020, 11, 30),
+    fr: new Date(2021, 0, 1),
+    sa: new Date(2021, 0, 2),
+    su: new Date(2021, 0, 3),
+    mo: new Date(2021, 0, 4),
+    tu: new Date(2021, 0, 5),
+    we: new Date(2021, 0, 6),
+    th: new Date(2021, 0, 7),
   };
   for (const input in expectations) {
     const output = await DateType.parse(input, locale);
