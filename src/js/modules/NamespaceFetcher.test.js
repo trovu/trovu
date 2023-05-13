f52b4f49cadec8bfb5f96ae43a417a4dd65f0db8
Georg Jähnig <georg@jaehnig.org>
2023-05-14 01:43:36 +0200

    Simplify test code, group, add 1-level test.

--- src/js/modules/NamespaceFetcher.test.js
+++ src/js/modules/NamespaceFetcher.test.js
@@ -1,39 +1,43 @@
 import NamespaceFetcher from './NamespaceFetcher.js';
 import jsyaml from 'js-yaml';
 
-test('NamespaceFetcher.processInclude', () => {
-  const expectations = new Map();
-  expectations.set(
-    {
-      shortcut: jsyaml.load(`
-        include:
-          key: fr-de 1
-          namespace: leo
-      `),
-      namespaceInfos: jsyaml.load(`
-        leo:
-          shortcuts:
-            de-fr 1:
-              url: https://dict.leo.org/französisch-deutsch/{%word}
-              title: Allemand-Français (leo.org)
-            fr-de 1:
-              title: Französisch-Deutsch (leo.org)
-              include:
-                key: de-fr 1
-      `),
-    },
-    {
+describe('NamespaceFetcher.processInclude', () => {
+  const namespaceInfos = jsyaml.load(`
+    leo:
+      shortcuts:
+        de-fr 1:
+          url: https://dict.leo.org/französisch-deutsch/{%word}
+          title: Allemand-Français (leo.org)
+        fr-de 1:
+          title: Französisch-Deutsch (leo.org)
+          include:
+            key: de-fr 1
+  `);
+
+  test('1 level', () => {
+    const shortcut = jsyaml.load(`
+    include:
+      key: de-fr 1
+    `);
+    expect(
+      new NamespaceFetcher({}).processInclude(shortcut, 'leo', namespaceInfos),
+    ).toEqual({
+      url: 'https://dict.leo.org/französisch-deutsch/{%word}',
+      title: 'Allemand-Français (leo.org)',
+    });
+  });
+
+  test('2 level', () => {
+    const shortcut = jsyaml.load(`
+    include:
+      key: fr-de 1
+      namespace: leo
+  `);
+    expect(
+      new NamespaceFetcher({}).processInclude(shortcut, '', namespaceInfos),
+    ).toEqual({
       url: 'https://dict.leo.org/französisch-deutsch/{%word}',
       title: 'Französisch-Deutsch (leo.org)',
-    },
-  );
-
-  for (const [input, expected] of expectations) {
-    const output = new NamespaceFetcher({}).processInclude(
-      input.shortcut,
-      '',
-      input.namespaceInfos,
-    );
-    expect(output).toEqual(expected);
-  }
+    });
+  });
 });
