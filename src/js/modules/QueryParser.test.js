7dc54518c6d321bb61a16cbbadc623cc539dc59c
Georg Jähnig <georg@jaehnig.org>
2023-05-14 22:11:51 +0200

    Add test for 2 args.

--- src/js/modules/QueryParser.test.js
+++ src/js/modules/QueryParser.test.js
@@ -1,6 +1,13 @@
 import QueryParser from './QueryParser.js';
 
 describe('QueryParser.parse', () => {
+  test('2 arguments', () => {
+    expect(QueryParser.parse('db b,hh')).toMatchObject({
+      keyword: 'db',
+      args: ['b', 'hh'],
+    });
+  });
+
   test('uppercase keyword', () => {
     expect(QueryParser.parse('G foobar')).toMatchObject({
       keyword: 'g',
