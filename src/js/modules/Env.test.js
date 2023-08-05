a24dbd93b19e78da3838aa7115821d52c2766bae
Georg Jähnig <georg@jaehnig.org>
2023-08-05 15:36:56 +0200

    Move.

--- src/js/modules/Env.test.js
+++ src/js/modules/Env.test.js
@@ -2,18 +2,18 @@ import Env from './Env.js';
 const env = new Env({});
 
 describe('Env.getCountryFromIP', () => {
-  env.fetchDbIp = jest.fn(() => {
-    return JSON.stringify({
-      ipAddress: '89.245.199.31',
-      continentCode: 'EU',
-      continentName: 'Europe',
-      countryCode: 'DE',
-      countryName: 'Germany',
-      stateProv: 'Hesse',
-      city: 'Frankfurt am Main',
-    });
-  });
   test('mocked fetch', async () => {
+    env.fetchDbIp = jest.fn(() => {
+      return JSON.stringify({
+        ipAddress: '89.245.199.31',
+        continentCode: 'EU',
+        continentName: 'Europe',
+        countryCode: 'DE',
+        countryName: 'Germany',
+        stateProv: 'Hesse',
+        city: 'Frankfurt am Main',
+      });
+    });
     expect(env.getCountryFromIp()).resolves.toEqual('DE');
   });
 });
