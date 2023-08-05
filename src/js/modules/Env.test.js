f4492aee7cc98e371c4253ddf0dbcb673dd3976e
Georg Jähnig <georg@jaehnig.org>
2023-08-05 15:56:52 +0200

    Move mock.

--- src/js/modules/Env.test.js
+++ src/js/modules/Env.test.js
@@ -1,7 +1,19 @@
 import Env from './Env.js';
 const env = new Env({});
 
-describe('Env.getLanguageAndCountryFromBrowser', () => {
+env.fetchDbIp = jest.fn(() => {
+  return JSON.stringify({
+    ipAddress: '89.245.199.31',
+    continentCode: 'EU',
+    continentName: 'Europe',
+    countryCode: 'DE',
+    countryName: 'Germany',
+    stateProv: 'Hesse',
+    city: 'Frankfurt am Main',
+  });
+});
+
+describe('Env.getDefaultLanguageAndCountry', () => {
   test('browser returns language and country', () => {
     const env = new Env();
     env.getNavigatorLanguage = jest.fn(() => {
@@ -16,17 +28,6 @@ describe('Env.getLanguageAndCountryFromBrowser', () => {
 
 describe('Env.getCountryFromIP', () => {
   test('mocked fetch', async () => {
-    env.fetchDbIp = jest.fn(() => {
-      return JSON.stringify({
-        ipAddress: '89.245.199.31',
-        continentCode: 'EU',
-        continentName: 'Europe',
-        countryCode: 'DE',
-        countryName: 'Germany',
-        stateProv: 'Hesse',
-        city: 'Frankfurt am Main',
-      });
-    });
     expect(env.getCountryFromIp()).resolves.toEqual('DE');
   });
 });
