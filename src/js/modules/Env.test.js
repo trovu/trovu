157b468c1cd38094c81e100d6ad68c3c4ef3938b
Georg Jähnig <georg@jaehnig.org>
2023-08-05 15:36:18 +0200

    Simpler mock.

--- src/js/modules/Env.test.js
+++ src/js/modules/Env.test.js
@@ -1,29 +1,18 @@
 import Env from './Env.js';
 const env = new Env({});
 
-// Mock the fetchDbIp function to return { foo: 'bar' }
-jest.mock('./Env.js', () => {
-  const originalModule = jest.requireActual('./Env.js');
-  return {
-    __esModule: true,
-    ...originalModule,
-    default: class EnvMock extends originalModule.default {
-      async fetchDbIp() {
-        return JSON.stringify({
-          ipAddress: '89.245.199.31',
-          continentCode: 'EU',
-          continentName: 'Europe',
-          countryCode: 'DE',
-          countryName: 'Germany',
-          stateProv: 'Hesse',
-          city: 'Frankfurt am Main',
-        });
-      }
-    },
-  };
-});
-
 describe('Env.getCountryFromIP', () => {
+  env.fetchDbIp = jest.fn(() => {
+    return JSON.stringify({
+      ipAddress: '89.245.199.31',
+      continentCode: 'EU',
+      continentName: 'Europe',
+      countryCode: 'DE',
+      countryName: 'Germany',
+      stateProv: 'Hesse',
+      city: 'Frankfurt am Main',
+    });
+  });
   test('mocked fetch', async () => {
     expect(env.getCountryFromIp()).resolves.toEqual('DE');
   });
