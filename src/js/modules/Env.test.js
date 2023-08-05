3e66e5a030caf1728964df1503d53e741483fd78
Georg Jähnig <georg@jaehnig.org>
2023-08-05 15:57:08 +0200

    Extend test.

--- src/js/modules/Env.test.js
+++ src/js/modules/Env.test.js
@@ -16,12 +16,18 @@ env.fetchDbIp = jest.fn(() => {
 describe('Env.getDefaultLanguageAndCountry', () => {
   test('browser returns language and country', () => {
     const env = new Env();
-    env.getNavigatorLanguage = jest.fn(() => {
-      return 'en-DE';
-    });
-    expect(env.getLanguageAndCountryFromBrowser()).toEqual({
+    env.getNavigatorLanguage = jest.fn(() => 'en-DE');
+    expect(env.getDefaultLanguageAndCountry()).resolves.toEqual({
       language: 'en',
       country: 'de',
+    });
+  });
+  test('browser returns only language', () => {
+    const env = new Env();
+    env.getNavigatorLanguage = jest.fn(() => 'en');
+    expect(env.getDefaultLanguageAndCountry()).resolves.toEqual({
+      language: 'en',
+      country: 'us',
     });
   });
 });
