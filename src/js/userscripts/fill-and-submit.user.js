f9852a6ddc1bf21bbc5020f1ba4711f796177ec7
Georg Jähnig <georg@jaehnig.org>
2023-11-05 03:33:18 +0100

    Also look for 'serchilo' params for backwards compatibility.

--- src/js/userscripts/fill-and-submit.user.js
+++ src/js/userscripts/fill-and-submit.user.js
@@ -17,15 +17,20 @@
   );
 
   // Process 'fill' params and set values.
+  // Also look for 'serchilo' params for backwards compatibility.
   for (const [key, value] of params) {
-    if (key.includes('[fill]')) {
+    if (
+      (key.startsWith('trovu') || key.startsWith('serchilo')) &&
+      key.includes('[fill]')
+    ) {
       const selector = key.match(/\[fill\]\[(.*?)\]$/)[1];
       const element = document.querySelector(selector);
       if (element) element.value = value;
     }
   }
   // Trigger 'submit' if specified.
-  const submitSelector = params.get('serchilo[submit]');
+  const submitSelector =
+    params.get('trovu[submit]') || params.get('serchilo[submit]');
   if (submitSelector) {
     const submitElement = document.querySelector(submitSelector);
     if (submitElement) submitElement.click();
