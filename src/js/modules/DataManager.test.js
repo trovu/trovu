72b4101a9d69aaddc41ea3c3f8e8d6cb6aa32682
Georg Jähnig <georg@jaehnig.org>
2023-11-18 23:04:13 +0100

    Add test for DataManager.

---
+++ src/js/modules/DataManager.test.js
@@ -0,0 +1,15 @@
+import DataManager from './DataManager';
+
+describe('DataManager.load', () => {
+  test('should return object with specific keys', () => {
+    const result = DataManager.load();
+    expect(result).toEqual(
+      expect.objectContaining({
+        shortcuts: expect.anything(),
+        types: expect.objectContaining({
+          city: expect.anything(),
+        }),
+      }),
+    );
+  });
+});
