618fc2a42f06d3c43b82bf85371adb3e5a98f7df
Georg Jähnig <georg@jaehnig.org>
2023-11-18 01:40:23 +0100

    Use data in type city.

--- src/js/modules/type/city.js
+++ src/js/modules/type/city.js
@@ -4,24 +4,15 @@ import Helper from '../Helper.js';
 export default class CityType {
   static async parse(str, env) {
     let country = env.country;
-    const fetchUrlTemplate =
-      'https://data.trovu.net/data/types/city/{%country}.yml';
     let matches;
-
     let abbreviation = str;
     if ((matches = str.match(/^(\w\w+)(\.)(.+)$/))) {
       [, country, , abbreviation] = matches;
     }
-
-    const fetchUrl = fetchUrlTemplate.replace('{%country}', country);
-
-    const citiesYml = await Helper.fetchAsync(fetchUrl, env);
-    if (citiesYml) {
-      const cities = jsyaml.load(citiesYml);
-      if (abbreviation in cities) {
-        const city = cities[abbreviation];
-        return city;
-      }
+    const cities = env.data.types.city[country];
+    if (abbreviation in cities) {
+      const city = cities[abbreviation];
+      return city;
     }
 
     return false;
