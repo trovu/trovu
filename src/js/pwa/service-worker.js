e07bde85b99b905a7d83a0180db20c10653f94c5
Georg Jähnig <georg@jaehnig.org>
2023-11-22 23:30:34 +0100

    Add scaffold.

---
+++ src/js/pwa/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'trovu';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/process/index.html',
+  '/includes/yourscript.js',
+  '/data.json'
+  // Add other assets
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Implement fetch event to handle requests
