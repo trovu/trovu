61a3a7e070d4385a185df3ba540bd5fcbb571777
Georg Jähnig <georg@jaehnig.org>
2023-11-23 15:53:47 +0100

    Refactor first functions.

--- src/js/cli.js
+++ src/js/cli.js
@@ -10,19 +10,23 @@ program.name('trovu').description('CLI for trovu.net').version('0.0.1');
 program
   .command('compile-data')
   .description('Compile YAML data files to JSON')
-  .action(() => {
-    const data = DataManager.load();
-    const json = JSON.stringify(data);
-    process.stdout.write(json);
-  });
+  .action(() => compileData);
+
+function compileData() {
+  const data = DataManager.load();
+  const json = JSON.stringify(data);
+  process.stdout.write(json);
+}
 
 program
   .command('normalize-data')
   .description('Normalize YAML data files')
-  .action(() => {
-    const data = DataManager.load();
-    DataManager.write(data);
-  });
+  .action(() => normalizeData);
+
+function normalizeData() {
+  const data = DataManager.load();
+  DataManager.write(data);
+}
 
 program
   .command('migrate-placeholders')
