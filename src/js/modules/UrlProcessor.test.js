343aa4b850d077f97b246dee14a882940c53e691
Georg Jähnig <georg@jaehnig.org>
2023-11-28 00:17:33 +0100

    Organize more tests.

--- src/js/modules/UrlProcessor.test.js
+++ src/js/modules/UrlProcessor.test.js
@@ -51,110 +51,116 @@ describe('UrlProcessor', () => {
     });
   });
 
-  test('getPlaceholdersFromString', async () => {
-    expect(
-      UrlProcessor.getPlaceholdersFromString('https://<query>', ''),
-    ).toEqual({
-      query: {
-        '<query>': {},
-      },
+  describe('getPlaceholderFromString', () => {
+    test('without attributes', async () => {
+      expect(
+        UrlProcessor.getPlaceholdersFromString('https://<query>', ''),
+      ).toEqual({
+        query: {
+          '<query>': {},
+        },
+      });
+    });
+
+    test('without attributes legacy', async () => {
+      expect(
+        UrlProcessor.getPlaceholdersFromStringLegacy('https://{%query}', '%'),
+      ).toEqual({
+        query: {
+          '{%query}': {},
+        },
+      });
+    });
+
+    test('with attributes', async () => {
+      expect(
+        UrlProcessor.getPlaceholdersFromString(
+          'https://<Start: { type: city }>',
+          '',
+        ),
+      ).toEqual({
+        Start: {
+          '<Start: { type: city }>': {
+            type: 'city',
+          },
+        },
+      });
+    });
+
+    test('with attributes legacy', async () => {
+      expect(
+        UrlProcessor.getPlaceholdersFromStringLegacy(
+          'https://{%Start|type=city}',
+          '%',
+        ),
+      ).toEqual({
+        Start: {
+          '{%Start|type=city}': {
+            type: 'city',
+          },
+        },
+      });
     });
   });
 
-  test('getPlaceholdersFromStringLegacy', async () => {
-    expect(
-      UrlProcessor.getPlaceholdersFromStringLegacy('https://{%query}', '%'),
-    ).toEqual({
-      query: {
-        '{%query}': {},
-      },
+  describe('getPlaceholderFromMatch', () => {
+    test('without attributes', async () => {
+      expect(UrlProcessor.getPlaceholderFromMatch([, 'query'])).toEqual({
+        name: 'query',
+        placeholder: {},
+      });
     });
-  });
 
-  test('getPlaceholdersFromString', async () => {
-    expect(
-      UrlProcessor.getPlaceholdersFromString(
-        'https://<Start: { type: city }>',
-        '',
-      ),
-    ).toEqual({
-      Start: {
-        '<Start: { type: city }>': {
+    test('without attributes legacy', async () => {
+      expect(UrlProcessor.getPlaceholderFromMatchLegacy([, 'query'])).toEqual({
+        name: 'query',
+        placeholder: {},
+      });
+    });
+
+    test('with attributes', async () => {
+      expect(
+        UrlProcessor.getPlaceholderFromMatch([, 'Start: { type: city}']),
+      ).toEqual({
+        name: 'Start',
+        placeholder: {
           type: 'city',
         },
-      },
+      });
     });
-  });
 
-  test('getPlaceholdersFromStringLegacy', async () => {
-    expect(
-      UrlProcessor.getPlaceholdersFromStringLegacy(
-        'https://{%Start|type=city}',
-        '%',
-      ),
-    ).toEqual({
-      Start: {
-        '{%Start|type=city}': {
+    test('with attributes legacy', async () => {
+      expect(
+        UrlProcessor.getPlaceholderFromMatchLegacy([, 'Start|type=city']),
+      ).toEqual({
+        name: 'Start',
+        placeholder: {
           type: 'city',
         },
-      },
+      });
     });
   });
 
-  test('getPlaceholderFromMatch', async () => {
-    expect(UrlProcessor.getPlaceholderFromMatch([, 'query'])).toEqual({
-      name: 'query',
-      placeholder: {},
+  describe('processAttributeEncoding', () => {
+    test('default', async () => {
+      expect(UrlProcessor.processAttributeEncoding({}, 'ÄÖÜäöü')).toEqual(
+        '%C3%84%C3%96%C3%9C%C3%A4%C3%B6%C3%BC',
+      );
     });
-  });
 
-  test('getPlaceholderFromMatchLegacy', async () => {
-    expect(UrlProcessor.getPlaceholderFromMatchLegacy([, 'query'])).toEqual({
-      name: 'query',
-      placeholder: {},
+    test('iso-8859-1', async () => {
+      expect(
+        UrlProcessor.processAttributeEncoding(
+          { encoding: 'iso-8859-1' },
+          'ÄÖÜäöü',
+        ),
+      ).toEqual('%C4%D6%DC%E4%F6%FC');
     });
-  });
 
-  test('getPlaceholderFromMatch', async () => {
-    expect(
-      UrlProcessor.getPlaceholderFromMatch([, 'Start: { type: city}']),
-    ).toEqual({
-      name: 'Start',
-      placeholder: {
-        type: 'city',
-      },
+    test('none', async () => {
+      expect(
+        UrlProcessor.processAttributeEncoding({ encoding: 'none' }, 'ÄÖÜäöü'),
+      ).toEqual('ÄÖÜäöü');
     });
-  });
-
-  test('getPlaceholderFromMatchLegacy', async () => {
-    expect(
-      UrlProcessor.getPlaceholderFromMatchLegacy([, 'Start|type=city']),
-    ).toEqual({
-      name: 'Start',
-      placeholder: {
-        type: 'city',
-      },
-    });
-  });
-
-  test('processAttributeEncoding default', async () => {
-    expect(UrlProcessor.processAttributeEncoding({}, 'ÄÖÜäöü')).toEqual(
-      '%C3%84%C3%96%C3%9C%C3%A4%C3%B6%C3%BC',
-    );
-  });
-
-  test('processAttributeEncoding iso-8859-1', async () => {
-    expect(
-      UrlProcessor.processAttributeEncoding(
-        { encoding: 'iso-8859-1' },
-        'ÄÖÜäöü',
-      ),
-    ).toEqual('%C4%D6%DC%E4%F6%FC');
-  });
-
-  test('processAttributeEncoding none', async () => {
-    expect(
-      UrlProcessor.processAttributeEncoding({ encoding: 'none' }, 'ÄÖÜäöü'),
-    ).toEqual('ÄÖÜäöü');
   });
 });
