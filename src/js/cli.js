a3662d8b79badc7b01515c7b6de9aff9701e98e0
Georg Jähnig <georg@jaehnig.org>
2023-11-27 17:44:18 +0100

    Refactor replacePlaceholders().

--- src/js/cli.js
+++ src/js/cli.js
@@ -49,52 +49,56 @@ function migratePlaceholders() {
           shortcut.deprecated.alternative.query.replace(/\{%(\d)\}/g, '<$1>');
       }
       if (shortcut.url) {
-        let newUrl = shortcut.url;
-        for (const prefix of ['%', '\\$']) {
-          const placeholders = UrlProcessor.getPlaceholdersFromStringLegacy(
-            shortcut.url,
-            prefix,
-          );
-          for (const placeholderName in placeholders) {
-            if (isOnlyNumber(placeholderName)) {
-              console.log(
-                `Warning: In shortcut ${namespace}.${key}, placeholder name ${placeholderName} is only a number.`,
-              );
-            }
-            let newPlaceholder;
-            const placeholder = placeholders[placeholderName];
-            const match = Object.keys(placeholder)[0];
-            const attributes = Object.values(placeholder)[0];
-            if (Object.keys(attributes).length === 0) {
-              newPlaceholder = placeholderName;
-            } else {
-              newPlaceholder = {};
-              newPlaceholder[placeholderName] = attributes;
-            }
-            const newPlaceholderYaml = jsyaml
-              .dump(newPlaceholder, {
-                flowLevel: 1,
-              })
-              .trim();
-            let newPlaceholderYamlBrackets = '<';
-            switch (prefix) {
-              case '%':
-                break;
-              case '\\$':
-                newPlaceholderYamlBrackets += '$';
-                break;
-            }
-            newPlaceholderYamlBrackets += `${newPlaceholderYaml}>`;
-            while (newUrl.includes(match)) {
-              newUrl = newUrl.replace(match, newPlaceholderYamlBrackets);
-            }
-          }
-        }
-        shortcut.url = newUrl;
+        replacePlaceholders(shortcut, namespace, key);
       }
     }
     DataManager.write(data);
   }
+}
+
+function replacePlaceholders(shortcut, namespace, key) {
+  let newUrl = shortcut.url;
+  for (const prefix of ['%', '\\$']) {
+    const placeholders = UrlProcessor.getPlaceholdersFromStringLegacy(
+      shortcut.url,
+      prefix,
+    );
+    for (const placeholderName in placeholders) {
+      if (isOnlyNumber(placeholderName)) {
+        console.log(
+          `Warning: In shortcut ${namespace}.${key}, placeholder name ${placeholderName} is only a number.`,
+        );
+      }
+      let newPlaceholder;
+      const placeholder = placeholders[placeholderName];
+      const match = Object.keys(placeholder)[0];
+      const attributes = Object.values(placeholder)[0];
+      if (Object.keys(attributes).length === 0) {
+        newPlaceholder = placeholderName;
+      } else {
+        newPlaceholder = {};
+        newPlaceholder[placeholderName] = attributes;
+      }
+      const newPlaceholderYaml = jsyaml
+        .dump(newPlaceholder, {
+          flowLevel: 1,
+        })
+        .trim();
+      let newPlaceholderYamlBrackets = '<';
+      switch (prefix) {
+        case '%':
+          break;
+        case '\\$':
+          newPlaceholderYamlBrackets += '$';
+          break;
+      }
+      newPlaceholderYamlBrackets += `${newPlaceholderYaml}>`;
+      while (newUrl.includes(match)) {
+        newUrl = newUrl.replace(match, newPlaceholderYamlBrackets);
+      }
+    }
+  }
+  shortcut.url = newUrl;
 }
 
 function isOnlyNumber(str) {
