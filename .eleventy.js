77298282d3ab6888469a709cd505db1b8d5ad24a
Georg Jähnig <georg@jaehnig.org>
2023-12-08 23:05:48 +0100

    Add first configs from eleventy-base-blog.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,17 @@
 const yaml = require('js-yaml');
 
+const { DateTime } = require('luxon');
+const markdownItAnchor = require('markdown-it-anchor');
+
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginBundle = require('@11ty/eleventy-plugin-bundle');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
+
+const pluginDrafts = require('./eleventy.config.drafts.js');
+const pluginImages = require('./eleventy.config.images.js');
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addDataExtension('yml', (contents) => yaml.load(contents));
   return {
