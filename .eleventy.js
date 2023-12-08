39af226091314a6c82bbe6a08ad96b3b8c005398
Georg Jähnig <georg@jaehnig.org>
2023-12-09 00:27:32 +0100

    Add more config.

--- .eleventy.js
+++ .eleventy.js
@@ -32,19 +32,35 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(pluginDrafts);
   eleventyConfig.addPlugin(pluginImages);
 
-  // Official plugins
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-    preAttributes: { tabindex: 0 },
-  });
-  eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-  eleventyConfig.addPlugin(pluginBundle);
-
   return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: 'njk',
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: 'njk',
+
+    // These are all optional:
     dir: {
-      input: './blog/',
+      input: './blog', // default: "."
+      includes: '../_includes', // default: "_includes"
+      data: '../_data', // default: "_data"
       output: './dist/blog',
     },
+
+    // -----------------------------------------------------------------
+    // Optional items:
+    // -----------------------------------------------------------------
+
+    // If your site deploys to a subdirectory, change `pathPrefix`.
+    // Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
+
+    // When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
+    // it will transform any absolute URLs in your HTML to include this
+    // folder name and does **not** affect where things go in the output folder.
+    pathPrefix: '/',
   };
 };
