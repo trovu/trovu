783f6d81316c092c0dd989b3729d244124de41a3
Georg Jähnig <georg@jaehnig.org>
2023-12-08 19:38:15 +0100

    Set dirs.

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,10 @@ const yaml = require('js-yaml');
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addDataExtension('yml', (contents) => yaml.load(contents));
+  return {
+    dir: {
+      input: 'blog',
+      output: 'dist/blog/',
+    },
+  };
 };
