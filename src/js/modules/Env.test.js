002eeb2eccfab6bcd91cef6439924f1b9e77a1be
Georg Jähnig <georg@jaehnig.org>
2023-12-30 05:05:54 +0100

    Add tests for getParams.

--- src/js/modules/Env.test.js
+++ src/js/modules/Env.test.js
@@ -6,6 +6,49 @@ const getUrlHashFooBar = () => {
 };
 
 describe('Env', () => {
+  describe('getParams', () => {
+    test('github', () => {
+      expect(new Env({ github: 'johndoe' }).getParams()).toEqual({
+        github: 'johndoe',
+      });
+    });
+    test('configUrl', () => {
+      expect(
+        new Env({ configUrl: 'https://example.com/config.yml' }).getParams(),
+      ).toEqual({
+        configUrl: 'https://example.com/config.yml',
+      });
+    });
+    test('language and country', () => {
+      expect(new Env({ language: 'en', country: 'us' }).getParams()).toEqual({
+        language: 'en',
+        country: 'us',
+      });
+    });
+    test('language, country and defaultKeyword', () => {
+      expect(
+        new Env({
+          language: 'en',
+          country: 'us',
+          defaultKeyword: 'example',
+        }).getParams(),
+      ).toEqual({
+        language: 'en',
+        country: 'us',
+        defaultKeyword: 'example',
+      });
+    });
+    test('debug', () => {
+      expect(new Env({ debug: true }).getParams()).toEqual({
+        debug: 1,
+      });
+    });
+    test('status', () => {
+      expect(new Env({ status: 'deprecated' }).getParams()).toEqual({
+        status: 'deprecated',
+      });
+    });
+  });
   describe('getDefaultLanguageAndCountry', () => {
     test('browser returns language and country', () => {
       const env = new Env();
