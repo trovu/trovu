5a45d4eeb7ab94d38f57f7b6ada707d0eb29e693
Georg Jähnig <georg@jaehnig.org>
2024-02-20 17:51:31 +0100

    Adjust service worker.

--- src/js/pwa/service-worker.js
+++ src/js/pwa/service-worker.js
@@ -1,18 +1,58 @@
-const CACHE_NAME = 'trovu';
+const CACHE_NAME = 'trovu-v1'; // Consider versioning your cache for easier updates
 const urlsToCache = [
   '/',
   '/index.html',
   '/process/index.html',
-  '/includes/yourscript.js', // Fix this with actual JS.
+  '/index.js',
+  '/process.js',
   '/data.json',
+  '/style.css',
+  '/main.js',
+  '/manifest.json',
+  '/favicon.ico',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  // Include additional essential assets as needed
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     }),
   );
 });
 
 // Implement fetch event to handle requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need
+        // to clone it so we have two streams.
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    }),
+  );
+});
