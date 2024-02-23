094dc0d617b616bac83277027116fd6b1506ad74
Georg Jähnig <georg@jaehnig.org>
2024-02-23 02:11:37 +0100

    Refactor setDisplays.

--- src/js/modules/Home.js
+++ src/js/modules/Home.js
@@ -103,20 +103,27 @@ export default class Home {
       this.queryInput.value.trim() === '' &&
       this.suggestions.selected === -1
     ) {
-      document.querySelector('nav.navbar').style.display = 'block';
-      document.querySelector('#intro').style.display = 'block';
-      document.querySelector('#alert').style.display = 'block';
-      document.querySelector('footer').style.display = 'block';
+      this.setDisplays(['nav.navbar', '#intro', '#alert', 'footer'], 'block');
       this.helpDiv.innerHTML = '';
     } else {
-      document.querySelector('nav.navbar').style.display = 'none';
-      document.querySelector('#intro').style.display = 'none';
-      document.querySelector('#alert').style.display = 'none';
-      document.querySelector('footer').style.display = 'none';
+      this.setDisplays(['nav.navbar', '#intro', '#alert', 'footer'], 'none');
       this.helpDiv.innerHTML =
         'Select with ⬆️ ⬇️ for examples, click on<span class="namespace">namespace</span>or <span class="tag">tag</span> to filter.';
     }
     Home.setHeights();
+  }
+
+  setDisplays(elements, value) {
+    for (const element of elements) {
+      this.setDisplay(element, value);
+    }
+  }
+  setDisplay(selector, value) {
+    const element = document.querySelector(selector);
+    if (!element) {
+      return;
+    }
+    element.style.display = value;
   }
 
   setLocationHash() {
