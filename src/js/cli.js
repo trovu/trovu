746a49e431ec98ba6df205ba14eee87774888d1c
Georg Jähnig <georg@jaehnig.org>
2024-02-24 03:07:03 +0100

    Refactor Migrator.

--- src/js/cli.js
+++ src/js/cli.js
@@ -1,6 +1,6 @@
 import DataManager from './modules/DataManager';
+import Migrator from './modules/Migrator';
 import ShortcutTester from './modules/ShortcutTester';
-import UrlProcessor from './modules/UrlProcessor';
 import ajv from 'ajv';
 import { Command } from 'commander';
 import fs from 'fs';
@@ -92,112 +92,12 @@ function testShortcuts(options) {
   shortcutTester.testShortcuts();
 }
 
-function migratePlaceholders(options) {
-  const dataPath = options.data;
-  const shortcutsPath = options.shortcuts;
-  const typesPath = options.types;
-  const filter = options.filter;
-  const data = DataManager.load(dataPath, shortcutsPath, typesPath, filter);
-  for (const namespace in data.shortcuts) {
-    for (const key in data.shortcuts[namespace]) {
-      let shortcut = data.shortcuts[namespace][key];
-      // if shortcut typeoff string, convert to object
-      if (typeof shortcut === 'string') {
-        shortcut = replacePlaceholders(shortcut, namespace, key);
-      }
-      if (shortcut.url) {
-        shortcut.url = replacePlaceholders(shortcut.url, namespace, key);
-      }
-      if (
-        shortcut.deprecated &&
-        shortcut.deprecated.alternative &&
-        shortcut.deprecated.alternative.query
-      ) {
-        shortcut.deprecated.alternative.query =
-          shortcut.deprecated.alternative.query.replace(/\{%(\d)\}/g, '<$1>');
-      }
-      if (shortcut.include && shortcut.include.key) {
-        shortcut.include.key = replacePlaceholders(
-          shortcut.include.key,
-          namespace,
-          key,
-        );
-      }
-      data.shortcuts[namespace][key] = shortcut;
-    }
-    DataManager.write(data, dataPath, shortcutsPath, typesPath);
-  }
-}
-
-function replacePlaceholders(str, namespace, key) {
-  for (const prefix of ['%', '\\$']) {
-    const placeholders = UrlProcessor.getPlaceholdersFromStringLegacy(
-      str,
-      prefix,
-    );
-    for (const placeholderName in placeholders) {
-      if (isOnlyNumber(placeholderName)) {
-        console.log(
-          `Warning: In shortcut ${namespace}.${key}, placeholder name ${placeholderName} is only a number.`,
-        );
-      }
-      let newPlaceholder;
-      const placeholder = placeholders[placeholderName];
-      const match = Object.keys(placeholder)[0];
-      const attributes = Object.values(placeholder)[0];
-      if (Object.keys(attributes).length === 0) {
-        newPlaceholder = placeholderName;
-      } else {
-        newPlaceholder = {};
-        newPlaceholder[placeholderName] = attributes;
-      }
-      const newPlaceholderYaml = jsyaml
-        .dump(newPlaceholder, {
-          flowLevel: 1,
-        })
-        .trim();
-      let newPlaceholderYamlBrackets = '<';
-      switch (prefix) {
-        case '%':
-          break;
-        case '\\$':
-          newPlaceholderYamlBrackets += '$';
-          break;
-      }
-      newPlaceholderYamlBrackets += `${newPlaceholderYaml}>`;
-      while (str.includes(match)) {
-        str = str.replace(match, newPlaceholderYamlBrackets);
-      }
-    }
-  }
-  return str;
-}
-
-function isOnlyNumber(str) {
-  return Number.isFinite(Number(str));
-}
-
 function migrateExamples(options) {
-  const data = DataManager.load(options);
-  for (const namespace in data.shortcuts) {
-    for (const key in data.shortcuts[namespace]) {
-      let shortcut = data.shortcuts[namespace][key];
-      // Normalize examples.
-      if (shortcut.examples && !Array.isArray(shortcut.examples)) {
-        const examples = [];
-        for (const [argumentString, description] of Object.entries(
-          shortcut.examples,
-        )) {
-          const example = {
-            arguments: argumentString,
-            description: description,
-          };
-          examples.push(example);
-        }
-        shortcut.examples = examples;
-      }
-      data.shortcuts[namespace][key] = shortcut;
-    }
-    DataManager.write(data, options);
-  }
+  const migrator = new Migrator();
+  migrator.migrateExamples(options);
+}
+
+function migratePlaceholders(options) {
+  const migrator = new Migrator();
+  migrator.migratePlaceholders(options);
 }
