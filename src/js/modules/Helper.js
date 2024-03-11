2077b9dea7bcf586bd2935593588949dec9d65d4
Georg Jähnig <georg@jaehnig.org>
2024-03-11 18:07:19 +0100

    Refactor getVersion.

--- src/js/modules/Helper.js
+++ src/js/modules/Helper.js
@@ -59,10 +59,10 @@ export default class Helper {
   }
 
   static logVersion() {
-    console.log(
-      'Trovu running version',
-      pkg.gitCommitHash.slice(0, 7),
-      pkg.gitDate,
-    );
+    console.log('Trovu running version', this.getVersion());
+  }
+
+  static getVersion() {
+    return `${pkg.gitCommitHash.slice(0, 7)} ${pkg.gitDate}`;
   }
 }
