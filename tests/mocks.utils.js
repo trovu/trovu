0f9a571505871b5064351542ac93b0db641fb32a
Georg Jähnig <georg@jaehnig.org>
2024-03-15 22:52:06 +0100

    Refactor mocks.

---
+++ tests/mocks.utils.js
@@ -0,0 +1,16 @@
+global.fetch = jest.fn((url) => {
+  if (url.includes('/testuser/trovu-data-user/master/config.yml')) {
+    return Promise.resolve({
+      status: 200,
+      text: () => Promise.resolve('defaultKeyword: g'),
+    });
+  } else if (url.includes('/testuser/trovu-data-user/master/shortcuts.yml')) {
+    return Promise.resolve({
+      status: 200,
+      text: () =>
+        Promise.resolve(
+          'keyword1 1: https://www.google.com/search?hl=en&q=keyword1%20{%query}&ie=utf-8',
+        ),
+    });
+  }
+});
