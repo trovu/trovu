f914b69a9aa4cc608a63b031cd19ee490d010943
Georg Jähnig <georg@jaehnig.org>
2024-05-23 16:22:13 +0200

    Import first methods.

--- src/js/modules/DictionarySetter.js
+++ src/js/modules/DictionarySetter.js
@@ -2,12 +2,35 @@ import DataManager from './/DataManager';
 import QueryParser from './QueryParser';
 import UrlProcessor from './UrlProcessor';
 import fs from 'fs';
+import jsyaml from 'js-yaml';
 
 export default class DictionarySetter {
   constructor() {}
 
   setDictionaries() {
     const data = DataManager.load();
+    const langs = DictionarySetter.getLanguageList();
+    const t = jsyaml.load(fs.readFileSync('src/yml/translations.yml', 'utf8'));
+    const dicts = DictionarySetter.getDictionaries();
     DataManager.write(data);
+  }
+
+  static getDictionaries() {
+    return jsyaml.load(fs.readFileSync('src/yml/dictionaries.yml', 'utf8'));
+  }
+
+  static getLanguageList() {
+    const langs = {};
+    const dirs = fs.readdirSync('./node_modules/languagelist/data/');
+    for (const dir of dirs) {
+      const lang = jsyaml.load(
+        fs.readFileSync(
+          `./node_modules/languagelist/data/${dir}/language.yaml`,
+          'utf8',
+        ),
+      );
+      langs[dir] = lang;
+    }
+    return langs;
   }
 }
