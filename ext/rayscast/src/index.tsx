57fd99d6e8fef846e28bd6dc4eec769f4ad7b52c
Georg Jähnig <georg@jaehnig.org>
2024-06-16 21:56:44 +0200

    Add intro.

--- ext/rayscast/src/index.tsx
+++ ext/rayscast/src/index.tsx
@@ -137,9 +137,21 @@ ${examples ? examples : ""}
             title="Welcome"
             detail={
               <List.Item.Detail
-                markdown="
-# sfsdf
-            "
+                markdown={`
+# Welcome to trovu.net
+
+_Web search as if from your command line: Trovu's shortcuts take you directly to the search results of other sites.
+[Read more](https://trovu.net/docs/), watch a [video](https://www.youtube.com/watch?v=gOUNhCion9M), or try the examples:_
+
+- [`g berlin`](https://trovu.net/process/index.html?#query=g%20berlin) Search Google for Berlin
+- [`w berlin`](https://trovu.net/process/index.html?#query=w%20berlin) Go to the Wikipedia article about Berlin
+- [`fr.w berlin`](https://trovu.net/process/index.html?#query=fr.w%20berlin) Go to the French Wikipedia article about Berlin
+- [`gd london, liverpool`](https://trovu.net/process/index.html?#query=gd%20london%2C%20liverpool) Search for a route on Google Directions from London to Liverpool
+- [`gfl ber, ibiza, fr, 28`](https://trovu.net/process/index.html?#query=gfl%20ber%2C%20ibiza%2C%20fr%2C%2028) Search on Google Flights for a flight from Berlin to Ibiza, leaving next Friday, returning on the 28th
+- [`wg berlin`](https://trovu.net/process/index.html?#query=wg%20berlin) Search Wikipedia for all mentions of "berlin" via Google
+- [`npm csv`](https://trovu.net/process/index.html?#query=npm%20csv) Search the Node Package Manager for modules projects about CSV
+
+            `}
               />
             }
           />
