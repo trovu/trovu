beffdec6d467b7f52d8f34ea536ad394d3db1061
Georg Jähnig <georg@jaehnig.org>
2024-06-16 20:38:49 +0200

    Different color when not reachable.

--- ext/rayscast/src/index.tsx
+++ ext/rayscast/src/index.tsx
@@ -166,7 +166,15 @@ ${examples}`
               key={`${suggestion.namespace}.${suggestion.keyword}.${suggestion.argumentCount}`}
               title={suggestion.keyword}
               subtitle={suggestion.argumentString}
-              accessories={[{ text: title }, { tag: { value: suggestion.namespace, color: "rgb(220, 53, 69)" } }]}
+              accessories={[
+                { text: title },
+                {
+                  tag: {
+                    value: suggestion.namespace,
+                    color: suggestion.reachable ? "rgb(255, 53, 69)" : "rgb(220, 220, 220)",
+                  },
+                },
+              ]}
               detail={
                 isShowingDetail ||
                 (true && (
