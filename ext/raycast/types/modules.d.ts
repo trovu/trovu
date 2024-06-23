2fcb1f71e36e13d508236eb0becfa37fe062896b
Georg Jähnig <georg@jaehnig.org>
2024-06-23 21:10:48 +0200

    Add type declaration.

---
+++ ext/raycast/types/modules.d.ts
@@ -0,0 +1,34 @@
+declare module '../../../src/js/modules/Env.js' {
+  interface Env {
+    populate(params: Record<string, string>): Promise<void>;
+    data: any; // You should replace `any` with the actual type if known
+  }
+
+  export default Env;
+}
+
+declare module '../../../src/js/modules/SuggestionsGetter.js' {
+  import Env from './Env';
+
+  interface Suggestion {
+    argumentCount: string;
+    argumentString: string;
+    arguments?: object;
+    description?: string;
+    examples?: object[];
+    key: string;
+    keyword: string;
+    namespace: string;
+    reachable?: boolean;
+    tags?: string[];
+    title?: string;
+    url: string;
+  }
+
+  class SuggestionsGetter {
+    constructor(env: Env);
+    getSuggestions(query: string): Suggestion[];
+  }
+
+  export default SuggestionsGetter;
+}
