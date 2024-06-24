3816f5849d28dd4058572995fdf82ecaebb16ad5
Georg Jähnig <georg@jaehnig.org>
2024-06-24 16:37:33 +0200

    Move.

--- ext/raycast/src/modules.d.ts
+++ ext/raycast/src/modules.d.ts
@@ -1,4 +1,4 @@
-declare module '../../../src/js/modules/Env.js' {
+declare module "../../src/js/modules/Env.js" {
   interface Env {
     populate(params: Record<string, string>): Promise<void>;
     data: any; // You should replace `any` with the actual type if known
@@ -7,8 +7,8 @@ declare module '../../../src/js/modules/Env.js' {
   export default Env;
 }
 
-declare module '../../../src/js/modules/SuggestionsGetter.js' {
-  import Env from './Env';
+declare module "../../src/js/modules/SuggestionsGetter.js" {
+  import Env from "./Env";
 
   interface Suggestion {
     argumentCount: string;
