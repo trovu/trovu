bc1f4181f204c5170f0fb9bbfa9c5dc7cd4efa40
Georg Jähnig <georg@jaehnig.org>
2024-06-28 21:42:35 +0200

    Double quotes also here.

--- src/js/userscripts/fill-and-submit.user.js
+++ src/js/userscripts/fill-and-submit.user.js
@@ -10,27 +10,21 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
+  "use strict";
 
-  const params = new URLSearchParams(
-    decodeURIComponent(window.location.hash.substring(1)),
-  );
+  const params = new URLSearchParams(decodeURIComponent(window.location.hash.substring(1)));
 
   // Process 'fill' params and set values.
   // Also look for 'serchilo' params for backwards compatibility.
   for (const [key, value] of params) {
-    if (
-      (key.startsWith('trovu') || key.startsWith('serchilo')) &&
-      key.includes('[fill]')
-    ) {
+    if ((key.startsWith("trovu") || key.startsWith("serchilo")) && key.includes("[fill]")) {
       const selector = key.match(/\[fill\]\[(.*?)\]$/)[1];
       const element = document.querySelector(selector);
       if (element) element.value = value;
     }
   }
   // Trigger 'submit' if specified.
-  const submitSelector =
-    params.get('trovu[submit]') || params.get('serchilo[submit]');
+  const submitSelector = params.get("trovu[submit]") || params.get("serchilo[submit]");
   if (submitSelector) {
     const submitElement = document.querySelector(submitSelector);
     if (submitElement) submitElement.click();
