c7b33874edc06a2c7a933911a9469fd4782f5d50
Georg Jähnig <georg@jaehnig.org>
2024-06-28 21:50:28 +0200

    Format.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,21 @@
 module.exports = {
-  'plugins': ['jest'],
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'jest/globals': true
+  plugins: ["jest"],
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
   },
-  'extends': 'eslint:recommended',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  'rules': {
-    'indent': [
-      'error',
-      2,
-      {SwitchCase: 1},
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'no-unused-vars': [
-      'error',
-      {argsIgnorePattern: '^_'}
-    ],
-    'quotes': [
-      'error',
-      'double',
-       {'avoidEscape': true}
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
-  }
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "linebreak-style": ["error", "unix"],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"],
+  },
 };
