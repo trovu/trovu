26ce41f5a1f6e3c3faaf95e0a1ee7898231a86fd
Georg Jähnig <georg@jaehnig.org>
2024-07-07 19:38:32 +0200

    Add getConfig.

--- src/js/modules/DataCompiler.js
+++ src/js/modules/DataCompiler.js
@@ -1,4 +1,6 @@
 import child_process from "child_process";
+import fs from "fs";
+import jsyaml from "js-yaml";
 
 export default class DataReporter {
   static getGitInfo() {
@@ -8,5 +10,16 @@ export default class DataReporter {
       commit: { hash: commitHash.toString().trim().slice(0, 7), date: date.toString().trim() },
     };
     return git;
+  }
+  static getConfig() {
+    const configText = fs.readFileSync("trovu.config.default.yml", "utf8");
+    const config = jsyaml.load(configText);
+    // Override with local config if it exists.
+    if (fs.existsSync("trovu.config.yml")) {
+      const localConfigText = fs.readFileSync("trovu.config.yml", "utf8");
+      const localConfig = jsyaml.load(localConfigText);
+      Object.assign(config, localConfig);
+    }
+    return config;
   }
 }
