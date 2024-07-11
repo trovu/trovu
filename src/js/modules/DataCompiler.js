cbf6561b94880760e813edfb6255b7e1d5590242
Georg Jähnig <georg@jaehnig.org>
2024-07-11 22:00:35 +0200

    Rename.

--- src/js/modules/DataCompiler.js
+++ src/js/modules/DataCompiler.js
@@ -4,10 +4,10 @@ import jsyaml from "js-yaml";
 
 export default class DataReporter {
   static getGitInfo() {
-    const commitHash = child_process.execSync("git rev-parse HEAD");
+    const hash = child_process.execSync("git rev-parse HEAD");
     const date = child_process.execSync("git show -s --format=%ci");
     const git = {
-      commit: { hash: commitHash.toString().trim().slice(0, 7), date: date.toString().trim() },
+      commit: { hash: hash.toString().trim().slice(0, 7), date: date.toString().trim() },
     };
     return git;
   }
