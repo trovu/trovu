369e63a6798d78fad03bbb2beb60f3167bdfbb61
Georg Jähnig <georg@jaehnig.org>
2024-10-08 16:22:50 +0200

    Remove old Helper func.

--- src/js/modules/Helper.test.js
+++ src/js/modules/Helper.test.js
@@ -3,7 +3,3 @@ import Helper from "./Helper.js";
 test("splitKeepRemainder", () => {
   expect(Helper.splitKeepRemainder("g foo, bar", " ", 2)).toEqual(["g", "foo, bar"]);
 });
-
-test("escapeRegExp", () => {
-  expect(Helper.escapeRegExp("foo.*bar[baz]")).toMatch("foo\\.\\*bar\\[baz\\]");
-});
