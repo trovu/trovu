d4b6ab3b2e90d53c1c089e111cbd90def45fa68a
Georg Jähnig <georg@jaehnig.org>
2024-11-04 23:04:57 +0100

    Fetch jsons.

--- src/web-ext/options.js
+++ src/web-ext/options.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   const [languageSelect, countrySelect, githubInput, saveOptionsButton] = [
     "language",
     "country",
@@ -6,6 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
     "save-options",
   ].map(document.getElementById.bind(document));
 
+  const response = await fetch(browser.runtime.getURL("/json/languages.en.min.json"));
+  const languages = await response.json();
+  for (const [code, name] of Object.entries(languages)) {
+    console.log(code, name);
+    const option = document.createElement("option");
+    option.value = code;
+    option.textContent = name;
+    languageSelect.appendChild(option);
+  }
   browser.storage.local.get(["language", "country", "github"]).then((result) => {
     languageSelect.value = result.language || "en";
     countrySelect.value = result.country || "US";
