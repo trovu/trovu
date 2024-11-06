7b177491d79f0d8a77eea51c15ed415bef637d3c
Georg Jähnig <georg@jaehnig.org>
2024-11-06 03:26:53 +0100

    Format.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,64 @@
-import jest from "eslint-plugin-jest";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import jest from "eslint-plugin-jest";
 import globals from "globals";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("eslint:recommended"), {
+export default [
+  ...compat.extends("eslint:recommended"),
+  {
     plugins: {
-        jest,
-        "@typescript-eslint": typescriptEslint,
+      jest,
+      "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...jest.environments.globals.globals,
-        },
+      globals: {
+        ...globals.browser,
+        ...jest.environments.globals.globals,
+      },
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
 
     rules: {
-        indent: ["error", 2, {
-            SwitchCase: 1,
-        }],
+      indent: [
+        "error",
+        2,
+        {
+          SwitchCase: 1,
+        },
+      ],
 
-        "linebreak-style": ["error", "unix"],
+      "linebreak-style": ["error", "unix"],
 
-        "no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
 
-        quotes: ["error", "double", {
-            avoidEscape: true,
-        }],
+      quotes: [
+        "error",
+        "double",
+        {
+          avoidEscape: true,
+        },
+      ],
 
-        semi: ["error", "always"],
+      semi: ["error", "always"],
     },
-}];
+  },
+];
