cbf38bab3405c7d27f07a0aedf6991101372408b
Georg Jähnig <georg@jaehnig.org>
2024-11-15 19:50:38 +0100

    Add settings needed after dir move.

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://127.0.0.1:8081/",
+    specPattern: "tests/cypress/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "tests/cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
