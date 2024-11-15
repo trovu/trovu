b746af3acd1b4fc6ae8ac21206f6243104b41448
Georg Jähnig <georg@jaehnig.org>
2024-11-15 19:50:06 +0100

    Format.

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8081/',
+    baseUrl: "http://127.0.0.1:8081/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
