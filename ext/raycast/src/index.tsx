dfcfe2ac39e2782820b95c1e61fe071f49ac0671
Georg Jähnig <georg@jaehnig.org>
2025-03-31 20:21:42 +0200

    Allow ts-nocheck.

--- ext/raycast/src/index.tsx
+++ ext/raycast/src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import { ActionPanel, Action, getPreferenceValues, List, showToast, Toast, open } from "@raycast/api";
 import { useState, useEffect } from "react";
