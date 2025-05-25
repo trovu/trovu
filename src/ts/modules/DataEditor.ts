fc6418db1910925a39bdb69e694cd67ff52422a9
Georg Jähnig <georg@jaehnig.org>
2025-05-26 01:42:07 +0200

    Set letter for next.

--- src/ts/modules/DataEditor.ts
+++ src/ts/modules/DataEditor.ts
@@ -12,7 +12,7 @@ export default class DataEditor {
   private add0arg(data: {}) {
     const namespace = "o";
     for (const key in data.shortcuts[namespace]) {
-      if (!key.startsWith("f")) {
+      if (!key.startsWith("h")) {
         continue;
       }
       if (!data.shortcuts[namespace][key].url) {
