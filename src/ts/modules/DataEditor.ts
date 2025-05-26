25c6ea1e12e5a20ae10d49a158a09253452c0fa9
Georg Jähnig <georg@jaehnig.org>
2025-05-27 00:35:55 +0200

    Move letter up.

--- src/ts/modules/DataEditor.ts
+++ src/ts/modules/DataEditor.ts
@@ -12,7 +12,7 @@ export default class DataEditor {
   private add0arg(data: {}) {
     const namespace = "o";
     for (const key in data.shortcuts[namespace]) {
-      if (!key.startsWith("h")) {
+      if (!key.startsWith("i")) {
         continue;
       }
       if (!data.shortcuts[namespace][key].url) {
