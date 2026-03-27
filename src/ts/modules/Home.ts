7135a13f54e7dd771c4ded492c94d394e75af5f4
Your Name <your_email@example.com>
2026-03-27 13:50:52 -0700

    fix: bind updateOpensearch in standalone PWA flow

--- src/ts/modules/Home.ts
+++ src/ts/modules/Home.ts
@@ -272,7 +272,7 @@ export default class Home {
   /**
    * Add and update Opensearch tag.
    */
-  updateOpensearch() {
+  updateOpensearch = () => {
     if (!this.env.language || !this.env.country) {
       return;
     }
