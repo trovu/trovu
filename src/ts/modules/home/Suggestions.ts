b5403c108b8951bb0afb689eca88993b1c8f0817
Georg Jähnig <georg@jaehnig.org>
2026-04-15 02:57:58 +0200

    Add hr, reorder.

--- src/ts/modules/home/Suggestions.ts
+++ src/ts/modules/home/Suggestions.ts
@@ -83,9 +83,11 @@ export default class Suggestions {
 
     li.append(
       this.getMain(suggestion),
-      this.getDescription(suggestion),
       this.getExamples(suggestion),
+      document.createElement("hr"),
+      this.getDescription(suggestion),
       this.getUrl(suggestion),
+      document.createElement("hr"),
       this.hasTag(suggestion, "needs-userscript") ? this.getNeedsUserscript() : "",
       this.hasTag(suggestion, "is-affiliate") ? this.getIsAffiliate() : "",
       this.getTools(suggestion),
