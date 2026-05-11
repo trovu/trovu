43fde7c6683e105d4b26ead1a1d22bf0046e3cb8
Georg Jähnig <georg@jaehnig.org>
2026-05-11 03:32:26 +0200

    Move to n.

--- src/ts/modules/DataEditor.ts
+++ src/ts/modules/DataEditor.ts
@@ -11,7 +11,7 @@ export default class DataEditor {
   private add0arg(data: AnyObject) {
     const namespace = "o";
     for (const key in data.shortcuts[namespace]) {
-      if (!key.startsWith("m")) {
+      if (!key.startsWith("n")) {
         continue;
       }
       if (!data.shortcuts[namespace][key].url) {
